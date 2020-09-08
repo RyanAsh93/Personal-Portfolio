1c06454b99b11d8d40c41f4613f06c3b5c48a17f
RyanAsh93 <blueheadryan93@yahoo.com>
2020-09-08 10:43:19 -0600

    fixed project link

--- client/src/Components/Projects.js
+++ client/src/Components/Projects.js
@@ -13,7 +13,7 @@ function Projects() {
       <p>Built a app where you can add items for your shopping needs</p>
       <hr/>
       <h4>Personal Project</h4>
-      <a target="_blank" href="https://pocket-shopping-list.herokuapp.com">Traise App</a>
+      <a target="_blank" href="https://traise.mybluemix.net/">Traise App</a>
       <p>Personal Project with brother, Started as a template but have been adding and removing multiple features</p>
     </div>
   )
