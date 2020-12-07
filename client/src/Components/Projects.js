89b49cc87e807c0a75f2660be315ffc7a1a7c41b
RyanAsh93 <blueheadryan93@yahoo.com>
2020-12-07 11:41:20 -0700

    added projects

--- client/src/Components/Projects.js
+++ client/src/Components/Projects.js
@@ -4,17 +4,25 @@ function Projects() {
   return (
     <div>
       <h1 style={styles.head} >Projects</h1>
+      <h4>Clients Website</h4>
+      <a target="_blank" href="https://lakepointinsurance.mybluemix.net">LakePoint Insurance Agency</a>
+      <p>Building a landing page for a local insurance company.</p>
+      <hr/>
+      <h4>Personal Project</h4>
+      <a target="_blank" href="https://hatchwaysassessment.mybluemix.net">Student List</a>
+      <p>Built an app using an API to filter, and display students infomation.</p>
+      <hr/>
       <h4>Final DevPoint Labs Group Project</h4>
       <a target="_blank" href="https://digital--glovebox.herokuapp.com">Digital Glove Box</a>
-      <p>I worked on the Car Profile page, Edit Vehicle Form, and landing page</p>
+      <p>I worked on the Car Profile page, Edit Vehicle Form, and landing page.</p>
       <hr/>
       <h4>Personal Project</h4>
       <a target="_blank" href="https://pocket-shopping-list.herokuapp.com">Shopping List App</a>
-      <p>Built a app where you can add items for your shopping needs</p>
+      <p>Built an app where you can add, delete, and cross off items for your shopping list.</p>
       <hr/>
       <h4>Personal Project</h4>
-      <a target="_blank" href="https://traise-dev.mybluemix.net/">Traise App</a>
-      <p>Personal Project with brother, Started as a template but have been adding and removing multiple features</p>
+      <a target="_blank" href="https://traise-dev.mybluemix.net">Traise App</a>
+      <p>Personal project with my brother, Started as a template but have been adding and removing multiple features.</p>
     </div> 
   )
 }
